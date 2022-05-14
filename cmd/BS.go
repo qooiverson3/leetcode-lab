a60dfe963b9d2a51d494196ce01381339d74e7ee
賴威勝 <sheng@laiweishengdeMacBook-Air.local>
2022-05-14 19:04:29 +0800

    finish #704 Binary search

--- cmd/BS.go
+++ cmd/BS.go
@@ -54,19 +54,30 @@ func search(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
 
+	if len(nums) == 0 {
+		return -1
+	}
+
+	if len(nums) == 1 {
+		if nums[0] == target {
+			return nums[0]
+		}
+		if nums[0] != target {
+			return -1
+		}
+	}
 	for left <= right {
 		mid := (left + right) / 2
 
-		if mid == target {
+		if nums[mid] == target {
 			return mid
 		}
-		if mid < target {
+		if nums[mid] < target {
 			left = mid + 1
 		}
-		if mid > target {
+		if nums[mid] > target {
 			right = mid - 1
 		}
-
 	}
 	return -1
 }
